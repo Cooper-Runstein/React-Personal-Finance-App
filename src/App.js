f2c6ebf5fb54a0f8a7ae91bcfa3fe8849665ad34
crunstein <cooperun@gmail.com>
2018-06-19 21:58:30 -0700

    Wired counter and footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Counter from './Components/Counter';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      savings: 0
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
+        <Counter />
+        <Footer />
       </div>
     );
   }
---
+++ src/Components/Counter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Counter = (props) =>{
+    return(
+        <div id="footer"><h3> Counter </h3></div>
+    )
+}
+
+export default Counter;
+
