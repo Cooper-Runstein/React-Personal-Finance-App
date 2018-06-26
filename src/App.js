5e94973d85f474c0121dfc7045d0f0cfbf7dc5cc
cooper-runstein <cooperun@gmail.com>
2018-06-25 19:13:56 -0700

    Debt hooked up to state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import './css/display.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Counter from './Components/Counter';
@@ -10,25 +11,46 @@ class App extends Component {
     super(props)
     this.state = {
       savings: 0,
-      initialSavings: 0,
+      'initialSavings': 0,
+      debt: 0,
+      initialDebt: 0
     }
   }
 
-    updateInitialSavings = (e)=>{
-      e.preventDefault();
-      this.setState({
-        initialSavings: e.target.value
-      })
-    }
+  updateInitialSavings = (e)=>{
+    e.preventDefault();
+    this.setState({
+      initialSavings: e.target.value
+    })
+  }
 
-    saveInitialSavings = (e) =>{
-      e.preventDefault();
-      let newSavings = this.state.initialSavings;
-      this.setState({
-        savings: newSavings,
-        initialSavings: 0 
-      })
-    }
+  saveInitialSavings = (e) =>{
+    e.preventDefault();
+    document.getElementById("initial_savings").style.display = "none";
+    document.getElementById("initial_debt").style.display = "block";
+    let newSavings = this.state.initialSavings;
+    this.setState({
+      savings: newSavings,
+      initialSavings: 0 
+    })
+  }
+  updateInitialDebt = (e)=>{
+    e.preventDefault();
+    this.setState({
+      initialDebt: e.target.value
+    })
+  }
+
+  saveInitialDebt = (e) =>{
+    e.preventDefault();
+    document.getElementById("initial_debt").style.display = "none";
+    let newDebt = this.state.initialDebt;
+    this.setState({
+      Debt: newDebt,
+      initialDebt: 0 
+    })
+  }
+ 
  
 
   render() {
@@ -38,7 +60,12 @@ class App extends Component {
         <Main 
           updateInitialSavings = {this.updateInitialSavings}
           initialSavings = {this.state.initialSavings}
-          saveInitialSavings = {this.saveInitialSavings}/>
+          saveInitialSavings = {this.saveInitialSavings}
+          initialDebt = {this.state.initialDebt}
+          updateInitialDebt = {this.updateInitialDebt}
+          saveInitialDebt = {this.saveInitialDebt}
+          
+          />
         <Counter 
           savings={this.state.savings}/>
         <Footer />
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -4,18 +4,34 @@ import PropTypes from 'prop-types'
 const Main = (props) =>{
     return(
         <div id="Main">
-        <form>
-            <input
-                placeholder="Enter Starting Savings"
-                value = {props.initalSavings}
-                onChange = {props.updateInitialSavings}
-            >
-            </input>
-            <button
-            onClick= {props.saveInitialSavings}
-            >Set Initial Savings
-            </button>
-        </form>
+        <div id="initial_savings">
+            <form>
+                <input
+                    placeholder="Enter Starting Savings"
+                    value = {props.initalSavings}
+                    onChange = {props.updateInitialSavings}
+                >
+                </input>
+                <button
+                onClick= {props.saveInitialSavings}
+                >Set Initial Savings
+                </button>
+            </form>
+        </div>
+        <div id="initial_debt">
+            <form>
+                <input
+                    placeholder="Enter Starting Debt"
+                    value = {props.initalDebt}
+                    onChange = {props.updateInitialDebt}
+                >
+                </input>
+                <button
+                onClick= {props.saveInitialDebt}
+                >Set Initial Debt
+                </button>
+            </form>
+        </div>
         </div>
     )
 }
